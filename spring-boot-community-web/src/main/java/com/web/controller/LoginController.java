ee0b941838b782f4f3e490db82168912375a7599
Hg-lab <siregy1222@gmail.com>
2023-01-04 23:54:08 +0900

    5장 스프링 부트 시큐리티+OAuth2 - 1.5.x 버전

---
+++ spring-boot-community-web/src/main/java/com/web/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.web.controller;
+
+import com.web.annotation.SocialUser;
+import com.web.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping(value = "/{facebook|google|kakao}/complete")
+    public String loginComplete(@SocialUser User user) {
+        return "redirect:/board/list";
+    }
+}
