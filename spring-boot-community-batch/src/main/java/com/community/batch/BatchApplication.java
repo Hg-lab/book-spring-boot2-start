2e484e5608670c87f866bcff59fa11fadce38f50
Hg-lab <siregy1222@gmail.com>
2023-01-26 21:37:28 +0900

    7장 스프링 부트 배치 - Job, Step, ItemReader 설정, BuilderFactory 의존성 주입

--- spring-boot-community-batch/src/main/java/com/community/batch/BatchApplication.java
+++ spring-boot-community-batch/src/main/java/com/community/batch/BatchApplication.java
@@ -1,9 +1,11 @@
 package com.community.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing // JobBuilderFactory, StepBuilderFactory 의존성 주입
 public class BatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(BatchApplication.class);
