dc3ded9d90b966729ab70b178831c852b1e9c80f
Hg-lab <siregy1222@gmail.com>
2023-02-18 16:15:24 +0900

    7장 스프링 부트 배치 - Job 실행과 종료 메세지를 출력하는 InactiveJobListener구현

---
+++ spring-boot-community-batch/src/main/java/com/community/batch/jobs/inactive/listener/InactiveJobListener.java
@@ -0,0 +1,19 @@
+package com.community.batch.jobs.inactive.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+@Slf4j
+public class InactiveJobListener implements JobExecutionListener {
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        log.info("Before Job");
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        log.info("After Job");
+    }
+}
